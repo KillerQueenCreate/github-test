e6d9dc1e4192d1f458c29a0139bb620d2d22528c
luna <1989158472@qq.com>
2021-05-13 17:06:11 +0800

    第二次提交：修改了程序

--- git-maven-project/src/main/java/com/intouch/spring/IOCTest.java
+++ git-maven-project/src/main/java/com/intouch/spring/IOCTest.java
@@ -8,8 +8,9 @@ import java.util.Date;
 public class IOCTest {
     public static void main(String[] args) {
         ApplicationContext context=new ClassPathXmlApplicationContext("applicationContext.xml");
-        Date now= (Date) context.getBean("now");
-        System.out.println("Now is "+now);
+        //Date now= (Date) context.getBean("now");
+        Date today=(Date) context.getBean("now");
+        System.out.println("Now is "+today);
 
 
     }
